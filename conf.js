1b7d2fad0bdd8284af085df979ee4f712d31acf7
saabismi <vilho.syvahuoko@gmail.com>
2021-06-26 18:11:39 +0300

    lisätty rekkareita, lisätty lista kaikille löydetyille rekkareille ja tehty hieman mobiiliystävällisemmäksi

--- conf.js
+++ conf.js
@@ -4,23 +4,33 @@ function addPlate(number) {
 
     elem.setAttribute("src", "http://syvis.net/rekkarit/images/" + number + ".jpg");
 
-    elem.style.maxWidth = "100%";
-    elem.style.maxHeight = "100%";
-
     let elemParent = elem.parentElement;
 
     let textElem = document.getElementById(number + "text");
 
     elemParent.removeChild(textElem);
 
+    const plateList = document.getElementById("platelist");
+
+    let createPlateListItem = document.createElement("li");
+    let textForPlateList = document.createTextNode(number);
+
+    createPlateListItem.appendChild(textForPlateList);
+    plateList.appendChild(createPlateListItem);
+
 }
 
 addPlate(1);
+addPlate(2);
 addPlate(3);
 addPlate(4);
 addPlate(5);
 addPlate(7);
+addPlate(8);
+addPlate(11);
+addPlate(13);
 addPlate(16);
 addPlate(99);
+addPlate(420);
 addPlate(998);
 addPlate(999);
